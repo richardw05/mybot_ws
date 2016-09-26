4a6d3ac08ff3d294cb2b5ee44b1421615d1861db
rpw <richardw05@gmail.com>
2016-09-26 00:13:37 -0700

    Basic differential drive model with sensors

---
+++ run_cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rostopic pub /cmd_vel geometry_msgs/Twist "linear:
+  x: 0.2
+  y: 0.0
+  z: 0.0
+angular:
+  x: 0.0
+  y: 0.0
+  z: 0.1" 
+
---
+++ run_rviz.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+roslaunch mybot_description mybot_rviz.launch 
